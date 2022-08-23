96f66bdae8a0c5706caa46e9961b122071200b57
Daniel Martínez <danieljmp07@gmail.com>
2022-08-23 10:47:36 -0400

    descriptions of drawing functionsfor accesibility

--- pacman/sketch.js
+++ pacman/sketch.js
@@ -11,23 +11,29 @@ function draw() {
   fill("white")
   square(200, 200, 100);
 
+  describe("a quarter of an indigo arc")
   fill("indigo")
   noStroke()
   arc(100, 100, 90, 90, 0, HALF_PI)
 
+  describe("maroon arc less a quarter in the upper-right corner")
   fill("maroon")
   arc(300, 100, 90, 90, 0, PI + HALF_PI)
 
+  describe("turquoise arc less a quarter in the bottom-left corner")
   fill("turquoise")
   arc(100, 300, 90, 90, PI, HALF_PI)
 
+  describe("lightsalmon arc, a closed semi-circle at upper-right corner")
   fill("lightsalmon")
   arc(300, 300, 90, 90, 0, PI + QUARTER_PI, CHORD)
 
+  describe("Pacman")
   fill("yellow")
   stroke("black")
   arc(200, 200, 90, 90, QUARTER_PI, PI + HALF_PI + QUARTER_PI, PIE)
 
+  describe("Pacman's eye")
   fill("black")
   circle(190, 180, 10)
 }
